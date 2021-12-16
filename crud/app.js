7223302c99bac9f529db042a272ac05c7752bc84
Xaurav18 <bajracharyasaurav22@gmail.com>
2021-12-16 17:27:53 +0545

    5th commit

--- crud/app.js
+++ crud/app.js
@@ -2,11 +2,26 @@ const express = require('express');
 
 const app = express();
 const port = 5000;
+const connectDb = require('./db/connect');
+//Require dotenv
+require('dotenv').config();
 
-app.listen(port, (req, res) => {
+//Middleware
+app.arguments(express.json());
+
+//Connection
+const start = async () => {
+  await connectDb(process.env.MONGO_CONNECT);
+  app.listen(port, (req, res) => {
+    console.log('your are listening to port:', port);
+  });
+};
+start();
+
+/*app.listen(port, (req, res) => {
   console.log('you are listening to port:', port);
 });
 
 app.get('/about', (req, res) => {
   res.status(200).send('All is well');
-});
+}); */
